d1ce65a76f3e7f08623fb0a904a0de2e3e972815
frank217 <dw18@rice.edu>
2019-03-02 16:22:56 -0600

    working lowerbound

--- src/controllers/course-controller.js
+++ src/controllers/course-controller.js
@@ -13,7 +13,6 @@ router.post("/", function (req, response) {
    var degree = body.degree;
 
     var res = {
-        
         courses: [
             {
                 "id": "1001",
@@ -28,11 +27,68 @@ router.post("/", function (req, response) {
                 "name": "COMP 215",
             },
         ],
-
     };
-
     response.json(res);
-   
 });
+
+
+router.post("/", function (req, response) {
+    /*
+    * POST /api/course
+    */
+   var body = req.body;
+   console.log("[POST /api/course] body: ", body);
+
+   var major = body.major;
+   var degree = body.degree;
+
+    var res = {
+        courses: [
+            {
+                "id": "1001",
+                "name": "COMP 140",
+            },
+            {
+                "id": "1002",
+                "name": "COMP 182",
+            },
+            {
+                "id": "1003",
+                "name": "COMP 215",
+            },
+        ],
+    };
+    response.json(res);
+});
+
+router.post("/api/courses", function (req, response) {
+    /*
+    * POST /api/course
+    */
+   var body = req.body;
+   console.log("[POST /api/course] body: ", body);
+
+   var major = body.major;
+   var degree = body.degree;
+
+    var res = {
+        courses: [
+            {
+                "id": "1001",
+                "name": "COMP 140",
+            },
+            {
+                "id": "1002",
+                "name": "COMP 182",
+            },
+            {
+                "id": "1003",
+                "name": "COMP 215",
+            },
+        ],
+    };
+    response.json(res);
+});
+
 
 module.exports = router;
