c982235054f24096384d81bc535583002b651e21
Jason <jhwang73>
2019-03-02 15:54:02 -0600

    starting point

--- src/startingPoint.js
+++ src/startingPoint.js
@@ -1,8 +1,44 @@
 
 
-// Assume proper course graph is loaded 
+function getSemesters(course_name, lowerbound, upperbound) {
+    var sems = []
+    var sem = class_hash[course_name];
+    if (sem == "0" || sem == "1") {
+        for (i=lowerbound; i<upperbound; i+= 2) {
+            sems.push(i);
+        }
+    } else {
+        for (i=lowerbound; i<upperbound; i++) {
+            sems.push(i);
+        }
+    }
+    return sems;
+}
 
-function getStartingPoint(major, degree, sem, taken) {
+// Return id, name, taken, sems
+function convert_to_output(courses) {
+    var output = [];
+    for (var course in courses) {
+        course_data = courses[course];
+        var course_output = {};
+        var taken = -1;
+        var sems = [];
+        if (course_data["type"] == "hardreq") {
+            var course_sem = course_data["sem"];
+            taken = course_sem;
+            sems.push(course_sem);
+        } else {
+            sems = getSemesters(course, course_data["lowerbound"], course_data["upperbound"]);
+        }
+        output.push({"name":course, "taken":taken, "sems":sems});
+    }
+    return output;
+}
+
+
+
+
+function get_starting_point(major, degree, sem, taken) {
     // Something like this, edit based on where graphs are and such
     var graph = get_graph(major, degree);
     var validation = validate(graph, sem, taken)
